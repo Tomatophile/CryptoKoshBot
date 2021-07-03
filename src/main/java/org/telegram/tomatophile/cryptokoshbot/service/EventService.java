96962013b9a30738d7aeb745d49574c8bf7c89a6
Stroev Artyom <stroev8artyom@gmail.com>
2021-07-03 11:44:45 +0300

    take messages from application.yml

--- src/main/java/org/telegram/tomatophile/cryptokoshbot/service/EventService.java
+++ src/main/java/org/telegram/tomatophile/cryptokoshbot/service/EventService.java
@@ -1,15 +1,22 @@
 package org.telegram.tomatophile.cryptokoshbot.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.tomatophile.cryptokoshbot.bot.CryptoKoshBot;
 import org.telegram.tomatophile.cryptokoshbot.pojo.Event;
 
+@Setter
 @Service
 @RequiredArgsConstructor
 public class EventService {
+    @Value("${telegram.bot.blank.updateEvent}")
+    private String updateEvent;
+    @Value("${telegram.bot.blank.fallEvent}")
+    private String fallEvent;
 
     private final CryptoKoshBot cryptoKoshBot;
 
@@ -17,7 +24,7 @@ public class EventService {
     public void sendUpdate(Event event){
         var sendMessage = new SendMessage();
         sendMessage.setChatId(event.getChatId());
-        sendMessage.setText(String.format("Стоимость %s составляет $%s", event.getFigi(), event.getPrice()));
+        sendMessage.setText(String.format(updateEvent, event.getFigi(), event.getPrice()));
 
         cryptoKoshBot.execute(sendMessage);
     }
@@ -26,7 +33,7 @@ public class EventService {
     public void sendFall(Event event){
         var sendMessage = new SendMessage();
         sendMessage.setChatId(event.getChatId());
-        sendMessage.setText(String.format("Стоимость %s упала, и теперь составляет $%s", event.getFigi(), event.getPrice()));
+        sendMessage.setText(String.format(fallEvent, event.getFigi(), event.getPrice()));
 
         cryptoKoshBot.execute(sendMessage);
     }
