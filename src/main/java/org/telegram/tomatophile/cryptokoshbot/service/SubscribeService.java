4a934e7a42667316bfc71e9f6cb1a7aa10e0ddf5
Stroev Artyom <stroev8artyom@gmail.com>
2021-07-03 11:49:08 +0300

    config currency listener url in application.yml

--- src/main/java/org/telegram/tomatophile/cryptokoshbot/service/SubscribeService.java
+++ src/main/java/org/telegram/tomatophile/cryptokoshbot/service/SubscribeService.java
@@ -8,8 +8,14 @@ import org.telegram.tomatophile.cryptokoshbot.pojo.Subscribe;
 
 @Service
 public class SubscribeService {
-    @Value("${currencyListener.url}")
+    @Value("${currencyListener.baseUrl}")
     private String currencyListenerUrl;
+    @Value("${currencyListener.subscribeUpdateUrl}")
+    private String subscribeUpdateUrl;
+    @Value("${currencyListener.unsubscribeUpdateUrl}")
+    private String unsubscribeUpdateUrl;
+    @Value("${currencyListener.subscribeFallUrl}")
+    private String subscribeFallUrl;
 
     private final RestTemplate restTemplate;
 
@@ -18,7 +24,7 @@ public class SubscribeService {
     }
 
     public void subscribeOnUpdateCurrency(String chatId, String figi) {
-        var url = currencyListenerUrl.concat("subscribe/update");
+        var url = currencyListenerUrl.concat(subscribeUpdateUrl);
 
         var subscribe = Subscribe.builder().chatId(chatId).figi(figi).build();
 
@@ -26,7 +32,7 @@ public class SubscribeService {
     }
 
     public void unsubscribeOnUpdateCurrency(String chatId, String figi) {
-        var url = currencyListenerUrl.concat("unsubscribe/update");
+        var url = currencyListenerUrl.concat(unsubscribeUpdateUrl);
 
         var subscribe = Subscribe.builder().chatId(chatId).figi(figi).build();
 
@@ -34,9 +40,9 @@ public class SubscribeService {
     }
 
     public void subscribeOnFallCurrency(String chatId, String figi, int fallPercent) {
-        var url = currencyListenerUrl.concat("subscribe/fall");
+        var url = currencyListenerUrl.concat(subscribeFallUrl);
 
-        var subscribe = Subscribe.builder().chatId(chatId).figi(figi).fallPercent(fallPercent);
+        var subscribe = Subscribe.builder().chatId(chatId).figi(figi).fallPercent(fallPercent).build();
 
         restTemplate.postForObject(url, subscribe, Subscribe.class);
     }
