5de6d29f50adc8f7574ff4d30cd12920c21f42b6
Stroev Artyom <stroev8artyom@gmail.com>
2021-07-03 11:44:02 +0300

    process for unknown command

--- src/main/java/org/telegram/tomatophile/cryptokoshbot/bot/CryptoKoshBot.java
+++ src/main/java/org/telegram/tomatophile/cryptokoshbot/bot/CryptoKoshBot.java
@@ -1,14 +1,16 @@
 package org.telegram.tomatophile.cryptokoshbot.bot;
 
 import lombok.Getter;
-import lombok.Setter;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.tomatophile.bottemplate.AbstractTelegramBot;
 import org.telegram.tomatophile.bottemplate.receiver.UpdateReceiver;
+import org.telegram.tomatophile.cryptokoshbot.service.ReplyService;
 
 @Getter
-@Setter
 @Component
 public class CryptoKoshBot extends AbstractTelegramBot {
     @Value("${telegram.bot.token}")
@@ -16,7 +18,32 @@ public class CryptoKoshBot extends AbstractTelegramBot {
     @Value("${telegram.bot.username}")
     private String botUsername;
 
-    public CryptoKoshBot(UpdateReceiver updateReceiver) {
+    @Value("${telegram.bot.blank.unknownCommand}")
+    private String unknownCommand;
+
+    private final ReplyService replyService;
+
+    private final UpdateReceiver updateReceiver;
+
+    public CryptoKoshBot(UpdateReceiver updateReceiver, ReplyService replyService) {
         super(updateReceiver);
+        this.updateReceiver = updateReceiver;
+        this.replyService = replyService;
+    }
+
+    @SneakyThrows
+    @Override
+    public void onUpdateReceived(Update update) {
+        var responseToUser = updateReceiver.receive(update);
+
+        if(!responseToUser.isEmpty()){
+            for (var send : responseToUser) {
+                if (send instanceof SendMessage) {
+                    execute((SendMessage) send);
+                }
+            }
+        } else {
+            execute(replyService.getTextMessage(update.getMessage().getChatId().toString(), unknownCommand));
+        }
     }
 }
